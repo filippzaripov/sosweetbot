84c8347b544c2eb202d6e1941ed9795a982e4745
philippsbucket <philippsbucket@gmail.com>
2019-08-01 21:04:33 +0300

    изменил обработку в зависимости от типа ответа (фото, клавиатура)

--- src/main/java/ru/sosweet/CallbackQueryHandler.java
+++ src/main/java/ru/sosweet/CallbackQueryHandler.java
@@ -2,20 +2,31 @@ package ru.sosweet;
 
 import org.apache.log4j.Logger;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.sosweet.factories.SendMessageKeyboardFactory;
 import ru.sosweet.keyboards.ButtonType;
+import ru.sosweet.photos.MenuPhotoReply;
+
+import java.util.Collections;
+import java.util.List;
 
 
-public class CallbackQueryHandler {
+class CallbackQueryHandler {
     private static final Logger log = Logger.getLogger(CallbackQueryHandler.class);
 
-    public static SendMessage replyQuery(Update update) {
-        long chat_id = update.getCallbackQuery().getMessage().getChatId();
+    static SendMessage replyQueryKeyboard(String callback, long chat_id) {
         return SendMessageKeyboardFactory
-                .getKeyboard(ButtonType.getButtonTypeByCallback(update
-                        .getCallbackQuery()
-                        .getData()))
+                .getKeyboard(ButtonType.getButtonTypeByCallback(callback))
                 .getKeyboardSendMessage(chat_id);
+    }
+
+    static List<SendPhoto> replyQueryPhoto(String callback, long chat_id) {
+        switch (ButtonType.getButtonTypeByCallback(callback)) {
+            case MENU:
+                return MenuPhotoReply.getSendPhotos(chat_id);
+            default:
+                return Collections.emptyList();
+        }
     }
 }
--- src/main/java/ru/sosweet/MainHandlerBot.java
+++ src/main/java/ru/sosweet/MainHandlerBot.java
@@ -24,44 +24,31 @@ public class MainHandlerBot extends SoSweetBot {
                 if (message_text.equals("/start")) {
                     execute(SendMessageKeyboardFactory.getKeyboard(MAIN).getKeyboardSendMessage(chat_id));
 
-                } else if (message_text.equals("/pic")) {
-                    SendPhoto msg = new SendPhoto()
-                            .setChatId(chat_id)
-                            .setPhoto("AgADAgAD3qoxGwluwEkGgisQumDD1T2ehQ8ABEbYNf20qqALvNMCAAEC")
-                            .setCaption("Photo");
-                    execute(msg); // Call method to send the photo
-
                 } else if (message_text.equals("/test")) {
                     execute(SendMessageKeyboardFactory
                             .getKeyboard(MAIN)
                             .getKeyboardSendMessage(chat_id));
-                } else if (message_text.equals("/menu")) {
-                    MenuPhotoReply.getSendPhotos(chat_id).forEach(photo -> {
-                        try {
-                            execute(photo);
-                        } catch (TelegramApiException e) {
-                            e.printStackTrace();
-                        }
-                    });
-
                 }
             }
         } else if (update.hasCallbackQuery()) {
-            //todo выделить содержимое метода в другой метод
             String callBack = update.getCallbackQuery().getData();
             long chat_id = update.getCallbackQuery().getMessage().getChatId();
 
-            if (ButtonType.getButtonTypeByCallback(callBack) == MENU) {
-                MenuPhotoReply.getSendPhotos(chat_id).forEach(photo -> {
-                    try {
-                        execute(photo);
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
-                });
-            } else {
-                execute(CallbackQueryHandler.replyQuery(update));
-            }
+            sendReply(callBack, chat_id);
+        }
+    }
+
+    private void sendReply(String callback, long chat_id) throws TelegramApiException {
+
+        switch (ButtonType.getButtonTypeByCallback(callback)) {
+            case MENU:
+                for (SendPhoto photo : CallbackQueryHandler.replyQueryPhoto(callback, chat_id)) {
+                    execute(photo);
+                }
+                break;
+            default:
+                execute(CallbackQueryHandler.replyQueryKeyboard(callback, chat_id));
+                break;
         }
     }
 }
