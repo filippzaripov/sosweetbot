7627ee1f23200545a09995b581b01c934ed4db0f
filippzaripov <philippsbucket@gmail.com>
2019-08-05 16:45:37 +0300

    fix

--- src/main/java/ru/sosweet/BotProperties.java
+++ src/main/java/ru/sosweet/BotProperties.java
@@ -9,18 +9,20 @@ public class BotProperties {
     private static final String contactsPath = "src/main/resources/text/contacts";
     private static final String promotionsPath = "src/main/resources/text/promotions";
 
-    public static final String PROXY_HOST;
-    public static final String PROXY_PORT;
-    public static final String PROXY_SET;
+    public static String PROXY_HOST;
+    public static String PROXY_PORT;
+    public static String PROXY_SET;
 
     public static final String ABOUT;
     public static final String CONTACTS;
     public static final String PROMOTIONS;
 
     static {
-        PROXY_HOST = proxyProperties.getProperty("socksProxyHost");
-        PROXY_PORT = proxyProperties.getProperty("socksProxyPort");
-        PROXY_SET = proxyProperties.getProperty("proxySet");
+        if (proxyProperties != null) {
+            PROXY_HOST = proxyProperties.getProperty("socksProxyHost");
+            PROXY_PORT = proxyProperties.getProperty("socksProxyPort");
+            PROXY_SET = proxyProperties.getProperty("proxySet");
+        }
 
         ABOUT = FilesReader.readText(aboutPath);
         CONTACTS = FilesReader.readText(contactsPath);
